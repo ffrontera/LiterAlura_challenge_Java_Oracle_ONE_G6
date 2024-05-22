24d4d63342a8cb81c8cf8eab48cbfd8b5d077bcd
ffrontera <ffrontera@live.com>
2024-05-22 15:36:44 -0300

    primer commit, inicializado Spring bot, con JPA y PostgreSQL drivers

---
+++ src/main/java/ff/literalura/LiterAluraChallengeJavaOracleOneG6Application.java
@@ -0,0 +1,13 @@
+package ff.literalura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiterAluraChallengeJavaOracleOneG6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiterAluraChallengeJavaOracleOneG6Application.class, args);
+	}
+
+}
---
+++ src/test/java/ff/literalura/LiterAluraChallengeJavaOracleOneG6ApplicationTests.java
@@ -0,0 +1,13 @@
+package ff.literalura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiterAluraChallengeJavaOracleOneG6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
